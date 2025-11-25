e59ffa417f315b7702b69f787fb9581fdc409e77
OvanindaGusdi <caesarvioovan@gmail.com>
2025-11-25 15:17:56 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBX6wMTyx15RUpqcT6k5oN5ffzbbERKPnE",
+    authDomain: "coba1-6651e.firebaseapp.com",
+    databaseURL: "https://coba1-6651e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "coba1-6651e",
+    storageBucket: "coba1-6651e.firebasestorage.app",
+    messagingSenderId: "41445988222",
+    appId: "1:41445988222:web:fe86f75578235366d3852f",
+    measurementId: "G-Z2B98T433M"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
